97457dfe646676db15c1a93f6766464be86bac25
fyunfei <623229343@qq.com>
2021-02-17 22:39:23 +0800

    feat:首页样式

---
+++ components/HomeCom.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="w-4/12 mb-10 float-left cursor-pointer select-none">
+    <div class="icon rounded-full my-0 mx-auto" :class="`icon-${img}`"></div>
+    <p class="title text-center">{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: String,
+    text: String,
+  },
+}
+</script>
+
+<style>
+.icon {
+  width: 100px;
+  height: 100px;
+  background-size: contain;
+}
+.icon-list {
+  background-image: url('@/assets/svg/list.svg');
+}
+.icon-plan {
+  background-position: -5px 6px;
+  background-image: url('@/assets/svg/plan.svg');
+}
+.icon-map {
+  background-image: url('@/assets/svg/map.svg');
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,40 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">travel_web</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <div class="container-bg"></div>
+    <div class="container z-10">
+      <div class="w-10/12 clearfix">
+        <HomeCom
+          v-for="app in appList"
+          :key="app.text"
+          :img="app.img"
+          :text="app.text"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      appList: [
+        {
+          text: '我的计划',
+          img: 'list',
+        },
+        {
+          text: '制定计划',
+          img: 'plan',
+        },
+        {
+          text: '地图',
+          img: 'map',
+        },
+      ],
+    }
+  },
+}
 </script>
 
 <style>
@@ -35,34 +43,23 @@ export default {}
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
+.container,
+.container-bg {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.container-bg {
+  background-image: url('@/assets/svg/list_.svg');
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center center;
 }
 </style>
